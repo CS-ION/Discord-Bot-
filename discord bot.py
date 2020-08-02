f04c31febc3cd0b2980c9948baab6fb4ecfdc9e6
Vivek Ashok <37447267+codephile1221@users.noreply.github.com>
2020-08-02 13:51:34 +0400

    update roasts

--- discord bot.py
+++ discord bot.py
@@ -100,34 +100,24 @@ async def Q(ctx,*args):
         
 @bot.command()
 async def roast(ctx,arg):
-        if 'krishna' in arg.lower():
-            await ctx.send('Smoking will kill you... meth will kill you... But, smoking meth will cure it.')
-        if 'vivek' in arg.lower():
-            await ctx.send('vivek got stuck in the shower because the instructions on the shampoo bottle said: Lather, Rinse, Repeat')
-        if 'ramanan' in arg.lower():
-            await ctx.send('What did the ramanan write on his Valentine card? You make me and my ice cream melt.')
-        if 'ramil' in arg.lower():
-            await ctx.send('why does ramil have white teeth ... because he loves to floss')
-        if 'harshal' in arg.lower():
-            await ctx.send('harshal has a new theory on inertia but it doesnt seem to go anywhere')
-        if 'rounak' in arg.lower():
-            await ctx.send('i am gorgeus and so am i a chirkut .... thats what she says')
-        if 'ayaan' in arg.lower():
-            await ctx.send('mere selmon bhoi ke bare me kya boli')
-        if 'nitishna' in arg.lower():
-            await ctx.send('my relationship with abi in NY was basically ''money heist'' ... IYKWIM')
-        if 'abishai' in arg.lower():
-            await ctx.send('absishais favourite tea .... penal-tea')
-        if 'jerin' in arg.lower():
-            await ctx.send('im a rider,provider,bring my bed now imma sleep right here')
-        if 'shane' in arg.lower():
-            await ctx.send('I will have a English Breakfast Tea with a pinch of sugar and a tinge of mint in it, you peasant worker')
-        if 'manav' in arg.lower():
-            await ctx.send('even my mom ignores me and only asks if u have any khakras left after i return from school')
-        if 'ujjwal' in arg.lower():
-            await ctx.send('naam he mera ujjwal babu aur me lollipop lagelu')
-        if 'vineet' in arg.lower():
-            await ctx.send('even mcdonalds had to shut down their farms coz you ate all their poultry')
+    roasts = {
+    'krishna': 'Smoking will kill you... meth will kill you... But, smoking meth will cure it.',
+    'vivek' : 'vivek got stuck in the shower because the instructions on the shampoo bottle said: Lather, Rinse, Repeat',
+    'ramanan' : 'What did the ramanan write on his Valentine card? You make me and my ice cream melt.',
+    'ramil' : 'why does ramil have white teeth ... because he loves to floss',
+    'harshal' : 'harshal has a new theory on inertia but it doesnt seem to go anywhere',
+    'rounak' : 'i am gorgeus and so am i a chirkut .... thats what she says',
+    'ayaan' : 'mere selmon bhoi ke bare me kya boli',
+    'nitishna' : 'my relationship with abi in NY was basically ''money heist'' ... IYKWIM',
+    'abishai':'absishais favourite tea .... penal-tea',
+    'jerin' : 'im a rider,provider,bring my bed now imma sleep right here',
+    'shane' : 'I will have a English Breakfast Tea with a pinch of sugar and a tinge of mint in it, you peasant worker',
+    'manav' : 'even my mom ignores me and only asks if u have any khakras left after i return from school',
+    'ujjwal' : 'naam he mera ujjwal babu aur me lollipop lagelu',
+    'vineet' : 'even mcdonalds had to shut down their farms coz you ate all their poultry'
+    }
+    
+    await ctx.send(roasts.get(arg.lower(),"kiska naam diya hai <:abeysaale:731486907208433724>"))
 
 @bot.command()
 async def dhanyavaad(ctx):
