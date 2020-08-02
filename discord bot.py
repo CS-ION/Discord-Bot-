c4aac4ffdc7f71baf5f6b0cfd4801682d5be601e
CS-ION <59445725+CS-ION@users.noreply.github.com>
2020-08-02 22:56:42 +0400

    Update discord bot.py

--- discord bot.py
+++ discord bot.py
@@ -14,15 +14,15 @@ async def on_message(message):
     if message.author == bot.user:
         return
     
-    if 'lol' or 'lmao' in message.content.lower():
+    if 'lol' in message.content.lower() or 'lmao' in message.content.lower():
         await message.channel.send('https://giphy.com/gifs/SalmanKhanFilms-lol-lmao-rofl-XHpoWfKOXwldWj6AqD')
-    if 'no' or 'never' in message.content.lower():
+    if 'no' in message.content.lower() or 'never' in message.content.lower():
         await message.channel.send('https://giphy.com/gifs/SalmanKhanFilms-no-nope-never-JPgrKbOaPUijm9CJOL')
     if 'stop' in message.content.lower():
         await message.channel.send('https://giphy.com/gifs/SalmanKhanFilms-angry-enough-thats-the-limit-S7FIMGqbQX9nxsoJjf')
-    if 'what does abishai say to nitishna' in message.content.lower():
+    if 'what does abi say to nitishna' in message.content.lower():
         await message.channel.send('https://giphy.com/gifs/SalmanKhanFilms-kiss-flirt-naughty-lTBAPZzEVx4EtSmq2X')
-    if 'nitishna' or 'prachi' in message.content.lower():
+    if 'nitishna' in message.content.lower() or 'prachi' in message.content.lower():
         await message.channel.send('https://giphy.com/gifs/bypriyashah-alia-bhatt-the-kapil-sharma-show-3ohfFjT9c0GPfGkZ0I')
     
     '''
